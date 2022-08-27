bac3c8dd65117c403f428d735543405a45942e24
Affan-Habib <affan.habib44@gmail.com>
2022-08-27 22:57:09 +0600

    defulat params with undefined values

--- defaultParams.js
+++ defaultParams.js
@@ -1,1 +1,18 @@
-console.log("Welcome to the functional world")
+const getDefault = (
+  a,
+  b = 4,
+  c = {
+    name: "affan",
+    professionl: "Javascript Developer",
+  }
+) => {
+  return console.log(a, b, c);
+};
+
+getDefault(3, undefined, {
+  name: undefined,
+  professionl: "UI/UX Developer",
+});
+
+// Expected Return
+// 3 4 { name: undefined, professionl: 'UI/UX Developer' }
