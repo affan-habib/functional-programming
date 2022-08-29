55763241f3acd78a1dcc9c043f9199a2baa4a626
affan <affan@eatlbd.com>
2022-08-29 12:44:17 +0600

    HOC

---
+++ higherOrderFunction.js
@@ -0,0 +1,11 @@
+const functionOne = (param1) => {
+    return(
+        function functionTwo(param2){
+            return param1 +" "+ param2
+        }
+    )
+}
+
+let functionThree = functionOne("Affan")
+let output = functionThree("is a Javascript developer")
+console.log(output)
