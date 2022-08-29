40ea8bb07cd753488025f4abf118a9fa858173ee
affan <affan@eatlbd.com>
2022-08-29 11:08:48 +0600

    for in loop

---
+++ forInLoop.js
@@ -0,0 +1,28 @@
+const obj = {
+    name : "Affan",
+    age : 30,
+    address : {
+        street : "Douglas Court",
+        city : "New York",
+    }
+}
+
+for(key in obj){
+    console.log("key", key)
+}
+//Expected Return
+// key name
+// key age
+// key address
+
+const person = {fname:"John", lname:"Doe", age:25};
+
+let text = "";
+for (let x in person) {
+  console.log(text += person[x]);
+}
+
+// Expected Return
+// John
+// JohnDoe
+// JohnDoe25
