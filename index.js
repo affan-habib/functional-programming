4507d19f6cc5791f7768295c3fb27b0a3edc6e27
affan <affan@eatlbd.com>
2022-09-13 16:05:29 +0600

    removing falsy

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ const objTwo = { name: "Java", age: 23 };
 const functionTwo = (object, property) => console.log(object[property]);
 functionTwo(objTwo, "age");
 
-const arr = [
-  { name: "Java", age: 2 },
-  { name: "Python", age: 3 },
-  { name: "Javascript", age: 4 },
-];
 
+const arr = ["f", false, -0, undefined, false, null]
 
+arr.length = 8
+
+console.log(arr)
+
+console.log(arr.map((a, index) => ( a || index)))
+
+// Expected Return [ 'f', 1, 2, 3, 4, 5, <2 empty items> ]
