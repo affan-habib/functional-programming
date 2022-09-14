b5d2b42946f9328c4b82675adb8e5b325f454b61
affan <affan@eatlbd.com>
2022-09-14 10:35:15 +0600

    default parms works

--- index.js
+++ index.js
@@ -24,16 +24,31 @@ console.log(arr.filter((el) => el >= 0));
 // Expected Return [ false, -0, false, null, 10 ]
 // it will check greater than 0 and also return all other items
 
-console.log(arr.filter(Boolean))
+console.log(arr.filter(Boolean));
 
-const filtered = arr.filter(Boolean)
-filtered[0] = "First One"
-console.log(filtered)
+const filtered = arr.filter(Boolean);
+filtered[0] = "First One";
+console.log(filtered);
 
-console.log(arr)
+console.log(arr);
 
-const findOne = (arr) => arr.find((el) => (typeof(el) == "string"))
+const findOne = (arr) => arr.find((el) => typeof el == "string");
 
-console.log(findOne(arr))
+console.log(findOne(arr));
 
-// Find Will return referece type data. if we change the object it will affect on main array. 
+// Find Will return referece type data. if we change the object it will affect on main array.
+
+const calculatePrice = (name = "Javascript", price = 100, vat = 0.1) =>
+  `${name} price is ${price + vat * 100}`;
+  console.log(calculatePrice("something", 10, 0.5));
+  console.log(calculatePrice(undefined, 10, 0.5));
+  console.log(calculatePrice(null, 10, 0.5));
+  
+  //Expected Return
+  // something price is 60
+  // Javascript price is 60
+  // null price is 60
+  // default param will not apply when null value found. To avoid this use null colesing
+  const calculatePriceTwo = (name = "Javascript", price = 100, vat = 0.1) =>
+  `${name ?? "Javascript"} price is ${price + vat * 100}`;
+  console.log(calculatePriceTwo(null, 10, 0.5));
