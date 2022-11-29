a3f9fa4953166ab75416956b92b53a2d6c0d83f6
affan <affan@eatlbd.com>
2022-11-29 12:35:33 +0600

    short circuit

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ console.log(sum(...numbers));
 
 console.log(sum.apply(null, numbers));
 
-
 const array = [1, 2, 3];
 const obj = { ...array }; // { 0: 1, 1: 2, 2: 3 }
 
@@ -17,7 +16,30 @@ function myFunction(x, y, z) {}
 const args = [0, 1, 2];
 myFunction.apply(null, args);
 
-
 // Unary plus (+)
 
+let a = 1;
+let b = 0;
 
+a &&= 2;
+console.log(a);
+// expected output: 2
+
+b &&= 2;
+console.log(b);
+// expected output: 0
+
+let arr = [12, 34, 67];
+
+arr.length &&= 2;
+
+console.log;
+
+const ob = {
+  name: "affan",
+  age: undefined,
+};
+
+ob.age ??= 23;
+
+console.log(ob);
